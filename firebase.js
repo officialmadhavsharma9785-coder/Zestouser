f3e0419e00d72e4fa955dab48bb2cfd7cda4e09a
officialmadhavsharma9785-coder <official.madhavsharma9785@gmail.com>
2026-07-20 12:29:57 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxzZVbB5RMjlZ-hZEXpBTRvNJuZpLclgg",
+  authDomain: "zesto-bd896.firebaseapp.com",
+  projectId: "zesto-bd896",
+  storageBucket: "zesto-bd896.firebasestorage.app",
+  messagingSenderId: "396537954129",
+  appId: "1:396537954129:web:28b76a6c5592f48b681d70"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
